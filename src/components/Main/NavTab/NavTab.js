68895dfbe99080d0b7c879137eb44a94c6bc1886
Taisiia Valieva <taya.skachkova@gmail.com>
2022-01-09 16:41:27 +0000

    Nav component added

---
+++ src/components/Main/NavTab/NavTab.js
@@ -0,0 +1,14 @@
+import './NavTab.css'
+
+function NavTab() {
+  return <nav className='nav'>
+  <ul className='nav-list'>
+      <li className='list-el'><button className='nav-button'>О проекте</button></li>
+      <li className='list-el'><button className='nav-button'>Технологии</button></li>
+      <li className='list-el'><button className='nav-button'>Студент</button></li>
+  </ul>
+</nav>
+}
+
+
+export default NavTab;
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,17 +1,12 @@
 import './Promo.css'
+import NavTab from '../NavTab/NavTab'
 
 function Promo() {
     return <div className='promo'>
         <h1 className='promo__header'>
         Учебный проект студента факультета Веб-разработки.
         </h1>
-        <nav className='promo__nav'>
-            <ul className='promo__nav-list'>
-                <li className='promo__list-el'><button className='promo__nav-button'>О проекте</button></li>
-                <li className='promo__list-el'><button className='promo__nav-button'>Технологии</button></li>
-                <li className='promo__list-el'><button className='promo__nav-button'>Студент</button></li>
-            </ul>
-        </nav>
+        <NavTab />
         </div>
 }
 
