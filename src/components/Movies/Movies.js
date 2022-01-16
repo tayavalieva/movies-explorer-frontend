9e5bbe8b5ce75411717e3625fd4a7dc7758403bf
Taisiia Valieva <taya.skachkova@gmail.com>
2022-01-16 17:45:56 +0000

    feat: preloader component added

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,12 +2,14 @@ import "./Movies.css";
 import SearchForm from "../SearchForm/SearchForm";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import Preloader from "../Preloader/Preloader";
 
 function Movies() {
   return (
     <section className="movies">
       <SearchForm />
       <FilterCheckbox />
+      <Preloader />
       <MoviesCardList />
       <button className="movies__more-button">Ещё</button>
     </section>
