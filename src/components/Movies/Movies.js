f8091132c4d9f530541987d1f66ac70637e5db26
Taisiia Valieva <taya.skachkova@gmail.com>
2022-01-16 15:02:29 +0000

    feat: Filter Checkbox component added

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,10 +1,12 @@
 import "./Movies.css";
 import SearchForm from "../SearchForm/SearchForm";
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
 function Movies() {
   return (
     <section className="movies">
       <SearchForm />
+      <FilterCheckbox />
     </section>
   );
 }
