5f6adb2c0b378347f1532b61001a92049532bded
Taisiia Valieva <taya.skachkova@gmail.com>
2022-01-17 19:32:57 +0000

    feat: logo routing to main page added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 import "./Logo.css";
 
 function Logo() {
-  return <div className="logo"></div>;
+  return (
+    <>
+      <Link to="/">
+        <div className="logo"></div>
+      </Link>
+    </>
+  );
 }
 
 export default Logo;
