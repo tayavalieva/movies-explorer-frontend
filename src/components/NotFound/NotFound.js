e2815c3a99f6a8a14906dd107a7e614fb1ad61c0
Taisiia Valieva <taya.skachkova@gmail.com>
2022-01-23 12:09:26 +0000

    feat: go back button added, useHistory hook added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,13 +1,21 @@
 import "./NotFound.css";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 function NotFound() {
+  const history = useHistory();
+
+  function handleBackButton() {
+    history.goBack();
+  }
+
   return (
     <div className="not-found">
       <h2 className="not-found__title">404</h2>
       <p className="not-found__content">Страница не найдена</p>
-      <a className="not-found__link" href="#">
+      <button className="not-found__button" onClick={handleBackButton}>
         Назад
-      </a>
+      </button>
     </div>
   );
 }
