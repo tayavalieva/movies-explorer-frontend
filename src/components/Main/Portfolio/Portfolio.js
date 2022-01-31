468f0bf48a8e5414c26f8c07dc52ced9d9ad51b7
Taisiia Valieva <taya.skachkova@gmail.com>
2022-01-31 21:37:54 +0000

    fix: links opening a new tab fixed

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -6,19 +6,31 @@ function Portfolio() {
       <h3 className="portfolio-title">Портфолио</h3>
       <ul className="portfolio__projects-list">
         <li className="portfolio__project-name">
-          <a href="https://yandex.ru/" className="portfolio__project-link">
+          <a
+            href="https://yandex.ru/"
+            className="portfolio__project-link"
+            target="_blank"
+          >
             <p className="portfolio__link-text ">Статичный сайт</p>
             <div className="portfolio__link-icon"></div>
           </a>
         </li>
         <li className="portfolio__project-name">
-          <a href="https://yandex.ru/" className="portfolio__project-link">
+          <a
+            href="https://yandex.ru/"
+            className="portfolio__project-link"
+            target="_blank"
+          >
             <p className="portfolio__link-text ">Адаптивный сайт</p>
             <div className="portfolio__link-icon"></div>
           </a>
         </li>
         <li className="portfolio__project-name">
-          <a href="https://yandex.ru/" className="portfolio__project-link">
+          <a
+            href="https://yandex.ru/"
+            className="portfolio__project-link"
+            target="_blank"
+          >
             <p className="portfolio__link-text">Одностраничное приложение</p>
             <div className="portfolio__link-icon"></div>
           </a>
