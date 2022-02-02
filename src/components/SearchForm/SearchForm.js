4562f3dab17c42dcbbb1842bf5903facfd805988
Taisiia Valieva <taya.skachkova@gmail.com>
2022-02-02 18:00:00 +0000

    feat: search form empty input validation added

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,19 +3,24 @@ import React, { useEffect, useState } from "react";
 
 function SearchForm(props) {
   const [searchedMovie, setSearchedMovie] = useState("");
+  const [error, setError] = useState("");
 
   function handleSearchMovie(e) {
     setSearchedMovie(e.target.value);
+    if (e.target.value.length < 1) {
+      setError("Нужно ввести ключевое слово");
+    }
+  }
+
+  function resetInput() {
+    setSearchedMovie("");
   }
 
   function handleSubmit(e) {
     e.preventDefault();
     props.onGetMovie(searchedMovie);
     resetInput();
-  }
-
-  function resetInput() {
-    setSearchedMovie("");
+    setError("");
   }
 
   return (
@@ -30,6 +35,7 @@ function SearchForm(props) {
           onChange={handleSearchMovie}
           required
         ></input>
+
         <button
           className="search-form__button"
           type="submit"
@@ -38,6 +44,7 @@ function SearchForm(props) {
           Найти
         </button>
       </form>
+      <span className="search-form__input-error">{error}</span>
     </section>
   );
 }
