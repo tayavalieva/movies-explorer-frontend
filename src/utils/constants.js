e4920846a7250adb25560be8e151a2d9fadf81b6
Taisiia Valieva <taya.skachkova@gmail.com>
2022-03-15 23:08:14 +0000

    fix: regex for name validation fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "https://api.nomoreparties.co";
 const apiURL = "https://api2.movie-explorer.nomoredomains.rocks/";
-export const nameRegex = /[a-zа-яё\s\-]/gi;
+export const nameRegex = /[a-zа-яё\s\-]+/i;
 export const shortMovieDuration = 40;
